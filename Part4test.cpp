7d1f7d27a7ca2b492e6ea77e254d8bd67bdb131b
ntdgy <ntdgy2001@gmail.com>
2022-06-07 08:09:03 +0000

    add Part4 QR break down and test code

---
+++ Part4test.cpp
@@ -0,0 +1,36 @@
+//
+// Created by dgy on 22-6-7.
+//
+#include <iostream>
+
+#include "Part4.cpp"
+using namespace std;
+int main() {
+    auto *A = new double[3 * 3];
+    // 1 2 2 2 1 2 1 2 1
+    A[0] = 1;
+    A[1] = 2;
+    A[2] = 2;
+    A[3] = 2;
+    A[4] = 1;
+    A[5] = 2;
+    A[6] = 1;
+    A[7] = 2;
+    A[8] = 1;
+    auto *Q = new double[3 * 3];
+    auto *R = new double[3 * 3];
+    QRBreakDown(A, 3, Q, R);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << Q[i * 3 + j] << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << R[i * 3 + j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
