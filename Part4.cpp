07825a2cb9e4d024069e1df044bb37c8d39e2654
ntdgy <ntdgy2001@gmail.com>
2022-06-12 10:14:11 +0000

    add codes

--- Part4.cpp
+++ Part4.cpp
@@ -1,64 +1,124 @@
 //#include "Part4.h"
 
 #include <cmath>
-#include <map>
 #include <cstring>
+#include <map>
 
 using namespace std;
 
+template <class T>
+void QRBreakDown(T *A, size_t n, T *Q_temp, T *R_temp) {
+  long long size = sizeof(T);
+  for (int k = 0; k < n; k++) {
+    T norm = 0;
+    for (int i = 0; i < n; i++) {
+      norm += A[i * n + k] * A[i * n + k];
+    }
+    norm = sqrt(norm);
+    R_temp[k * n + k] = norm;
+    for (int i = 0; i < n; i++) {
+      Q_temp[i * n + k] = A[i * n + k] / norm;
+    }
+    for (int j = k + 1; j < n; j++) {
+      T sum = 0;
+      for (int i = 0; i < n; i++) {
+        sum += Q_temp[i * n + k] * A[i * n + j];
+      }
+      R_temp[k * n + j] = sum;
+      for (int i = 0; i < n; i++) {
+        A[i * n + j] -= Q_temp[i * n + k] * sum;
+      }
+    }
+  }
+}
 
 template <class T>
-void QRBreakDown(T* A, size_t n, T* Q_temp, T* R_temp) {
-    long long size = sizeof(T);
-    for (int k = 0; k < n; k++) {
-        T norm = 0;
-        for (int i = 0; i < n; i++) {
-            norm += A[i * n + k] * A[i * n + k];
-        }
-        norm = sqrt(norm);
-        R_temp[k * n + k] = norm;
-        for (int i = 0; i < n; i++) {
-            Q_temp[i * n + k] = A[i * n + k] / norm;
-        }
-        for (int j = k + 1; j < n; j++) {
-            T sum = 0;
-            for (int i = 0; i < n; i++) {
-                sum += Q_temp[i * n + k] * A[i * n + j];
-            }
-            R_temp[k * n + j] = sum;
-            for (int i = 0; i < n; i++) {
-                A[i * n + j] -= Q_temp[i * n + k] * sum;
-            }
-        }
-    }
-}
-
-template<class T>
 long long determinant(T *matrix, int n) {
-    long long det = 0;
-    auto submatrix = new T[(n - 1) * (n - 1)];
-    if (n == 2)
-        return ((matrix[0] * matrix[3]) - (matrix[2] * matrix[1]));
-    else {
-        for (int x = 0; x < n; x++) {
-            int subi = 0;
-            for (int i = 1; i < n; i++) {
-                int subj = 0;
-                for (int j = 0; j < n; j++) {
-                    if (j == x) {
-                        continue;
-                    }
-                    submatrix[subi * (n-1) + subj] = matrix[i * n + j];
-                    subj++;
-                }
-                subi++;
-            }
-            det = det + (pow(-1, x) * matrix[x] * determinant(submatrix, n - 1));
+  long long det = 0;
+  auto submatrix = new T[(n - 1) * (n - 1)];
+  if (n == 2)
+    return ((matrix[0] * matrix[3]) - (matrix[2] * matrix[1]));
+  else {
+    for (int x = 0; x < n; x++) {
+      int subi = 0;
+      for (int i = 1; i < n; i++) {
+        int subj = 0;
+        for (int j = 0; j < n; j++) {
+          if (j == x) {
+            continue;
+          }
+          submatrix[subi * (n - 1) + subj] = matrix[i * n + j];
+          subj++;
         }
+        subi++;
+      }
+      det = det + (pow(-1, x) * matrix[x] * determinant(submatrix, n - 1));
     }
-    return det;
+  }
+  return det;
 }
 
+template <class T>
+long long trace(T *matrix, int n) {
+  long long trace = 0;
+  for (int i = 0; i < n; i++) {
+    trace += matrix[i * n + i];
+  }
+  return trace;
+}
+
+template <class T>
+bool inverse(int n, T *aa, T *re) {
+  double det_aa = determinant(aa, n);
+  if (det_aa == 0) {
+    throw NoInverseMatrix();
+    return false;
+  }
+  T *adjoint = new T[n * n];
+  T *bb = new T[(n - 1) * (n - 1)];
+
+  int pi, pj, q;
+  for (int ai = 0; ai < n; ai++) {
+    for (int aj = 0; aj < n; aj++) {
+      for (int bi = 0; bi < n - 1; bi++) {
+        for (int bj = 0; bj < n - 1; bj++) {
+          if (ai > bi) {
+            pi = 0;
+          } else {
+            pi = 1;
+          }
+          if (aj > bj) {
+            pj = 0;
+          }
+          else {
+            pj = 1;
+          }
+          bb[bi * (n - 1) + bj] = aa[(bi + pi) * n + bj + pj];
+        }
+      }
+      if ((ai + aj) % 2 == 0)
+        q = 1;
+      else
+        q = (-1);
+      adjoint[ai * n + aj] = q * determinant(bb, n - 1);
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      T tem = adjoint[i * n + j];
+      adjoint[i * n + j] = adjoint[j * n + i];
+      adjoint[j * n + i] = tem;
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      re[i * n + j] = adjoint[i * n + j] / det_aa;
+    }
+  }
+  delete[] adjoint;
+  delete[] bb;
+  return true;
+}
 
 /**
  * @brief 求实对称矩阵的特征值及特征向量的雅克比法
@@ -72,8 +132,8 @@ long long determinant(T *matrix, int n) {
  * @param pdbEigenValues			特征值数组
  * @return
  */
-bool JacbiCor(double* pMatrix, int nDim, double* pdblVects,
-              double* pdbEigenValues, double dbEps, int nJt) {
+bool JacbiCor(double *pMatrix, int nDim, double *pdblVects,
+              double *pdbEigenValues, double dbEps, int nJt) {
   for (int i = 0; i < nDim; i++) {
     pdblVects[i * nDim + i] = 1.0f;
     for (int j = 0; j < nDim; j++) {
@@ -168,7 +228,7 @@ bool JacbiCor(double* pMatrix, int nDim, double* pdblVects,
     mapEigen.insert(make_pair(pdbEigenValues[i], i));
   }
 
-  double* pdbTmpVec = new double[nDim * nDim];
+  double *pdbTmpVec = new double[nDim * nDim];
   std::map<double, int>::reverse_iterator iter = mapEigen.rbegin();
   for (int j = 0; iter != mapEigen.rend(), j < nDim; ++iter, ++j) {
     for (int i = 0; i < nDim; i++) {
